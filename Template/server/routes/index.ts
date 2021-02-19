da5588f0418bf573c8028eac4e95048127243a0a
Xitric <KasperSchultzDavidsen@gmail.com>
2021-02-19 09:44:47 +0000

    Migrated template app to TypeScript

--- Template/server/routes/index.ts
+++ Template/server/routes/index.ts
@@ -1,16 +1,17 @@
-const express = require('express');
-const http = require('superagent');
-var router = express.Router();
+import {Router} from 'express';
+import * as http from 'superagent';
+var router = Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
     const msg = await http.get('http://service_hello:3000/');
-    const info = await http.get('http://service_info:3000/')
+    const info = await http.get('http://service_info:3000/');
     res.render('index', { title: 'Express', message: msg.text, info: info.text });
   } catch (err) {
     console.error(err);
     res.render('index', { title: 'Express', message: 'Internal server error' });
   }
 });
-module.exports = router;
+
+export default router;
